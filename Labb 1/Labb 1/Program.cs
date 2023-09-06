1c0a6da424a0bc5efa12a15d5538bf164ecdf8a9
HesperisNivea <143076071+HesperisNivea@users.noreply.github.com>
2023-09-06 12:55:22 +0200

    test

---
+++ Labb 1/Labb 1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+string userInput = "29535123p48723487597645723645"; // how to get it thru args[]
+
+List<string> sequenseSellection = new List<string>();
+
+for (int i = 0; i < userInput.Length; i++)
+{
+    string sequenceCollector = "";
+    int sequenceMatchPosition = FoundSequenceMatchPosition(i, userInput); //if no math returns 'i' value
+    if (sequenceMatchPosition == i)
+    {
+        //Console.WriteLine(i + " " + sequenceMatchPosition);
+        //continue; 
+    }
+    else
+    {
+        for (int j = 0; j < i; j++)
+        {
+            Console.Write(userInput[j]);
+        }
+
+        for (int k = i; k <= sequenceMatchPosition; k++)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(userInput[k]);
+            sequenceCollector += userInput[k];
+            // ++++ collect a sequence into a string variable  ==> then when its ready Add to the List<>
+        }
+        sequenseSellection.Add(sequenceCollector); // collects value
+        Console.ResetColor();
+
+        for (int l = sequenceMatchPosition + 1; l < userInput.Length; l++)
+        {
+
+            Console.Write(userInput[l]);
+        }
+        Console.WriteLine();
+
+
+    }
+
+}
+Console.WriteLine("---------------------------------------------------------------------------------");
+Console.WriteLine("---------------------------------------------------------------------------------");
+Console.WriteLine("---------------------------------------------------------------------------------");
+
+int counter = 0;
+long sum = 0;
+foreach (string s in sequenseSellection)
+{
+    counter++;
+    sum += long.Parse(s);
+    Console.WriteLine("{0}. {1}", counter, s);
+}
+Console.WriteLine("---------------------------------------------------------------------------------");
+Console.WriteLine("Summan ar:" + sum);
+Console.WriteLine("---------------------------------------------------------------------------------");
+static int FoundSequenceMatchPosition(int checkpiontLocation, string userInput)
+{
+    char checkpointValue = userInput[checkpiontLocation];
+    if (!char.IsDigit(checkpointValue)) // if needed separate into else ifs  
+    {
+        //Console.WriteLine("x");
+        return checkpiontLocation;
+
+    }
+    //else if(checkpointValue == userInput[userInput.Length - 1])
+    //{
+    //    Console.WriteLine("y");
+    //    return checkpiontLocation;
+    //}
+    //else if (checkpointValue == userInput[checkpiontLocation + 1])
+    //{
+    //    Console.WriteLine("z");
+    //    return checkpiontLocation;
+    //}
+    else
+    {
+        for (int i = checkpiontLocation + 2; i < userInput.Length; i++)  // check if its going to be easier with  just adding two instead of one (in case of exception of upper if-sats ) 
+        {
+            if (!char.IsDigit(userInput[i - 1]))
+            {
+                break;
+            }
+            else if (!char.IsDigit(userInput[i]))
+            {
+                //Console.WriteLine("Not a numeric character!!!!!");
+                break;
+            }
+            else if (char.IsDigit(userInput[i]) && checkpointValue == userInput[i])
+            {
+                checkpiontLocation = i;
+                break;
+            }
+        }
+
+        return checkpiontLocation;
+    }
+
+}
+
+
---
+++ Tärningsspel/Tärningsspel/Program.cs
@@ -0,0 +1,53 @@
+﻿
+int[] players = new int[4] { 0, 0, 0, 0 }; // 
+int turn = 6;
+Turn(players);
+//while (turn > 0)
+//{
+
+//}
+static void Turn(int[] player)
+{
+    Random dice = new Random();
+    
+    int[] playerDiceScore = new int[player.Length];
+
+    for (int i = 0; i < player.Length; i++)
+    {
+        int diceScore = dice.Next(7);
+        player[i] += diceScore;
+        playerDiceScore[i] = diceScore;
+    }
+
+    Calculate(playerDiceScore);
+
+}
+
+static void Calculate(int[] diceScore)
+{
+    int[] winner = new int[4] { diceScore[0], diceScore[1], diceScore[2], diceScore[3] };
+
+    for (int i = 0; i < winner.Length - 1; i++)
+    {
+
+        for (int j = 0; j < winner.Length - 1; j++)
+        {
+
+
+            if (winner[j] > winner[j + 1])
+            {
+                int potentialWinner = winner[j + 1];
+                winner[j + 1] = winner[j];
+                winner[j] = potentialWinner;
+            }
+
+
+        }
+
+    }
+
+    foreach (int element in winner)
+    { Console.WriteLine(element); }
+
+    Console.WriteLine(diceScore[3]);
+}
