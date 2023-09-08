601d3df39dcbd94d664e089e9010b35b477fabcb
HesperisNivea <143076071+HesperisNivea@users.noreply.github.com>
2023-09-08 13:18:52 +0200

    update

--- Exercises v.35-2023/Exercises v.35-2023/Program.cs
+++ Exercises v.35-2023/Exercises v.35-2023/Program.cs
@@ -1,4 +1,4 @@
-﻿Ex15();
+﻿Ex18();
 
 void Ex10()
 {
@@ -15,7 +15,7 @@ void Ex11()
 {
     string[] numbers = new string[] { "noll", "ett", "tva", "tre", "fyra", "fem", "fem", "sex", "sju", "atta", "nio" };
     Console.WriteLine("Write a number: ");
-    
+
 
     if (Int32.TryParse(Console.ReadLine(), out int result))
     {
@@ -37,8 +37,8 @@ void Ex12()
     {
         Console.WriteLine("Write a number nr.1: ");
         int number = Int32.Parse(Console.ReadLine());
-        numbers[lenghtOfArray -i -1] = number;
-        
+        numbers[lenghtOfArray - i - 1] = number;
+
     }
 
     foreach (int num in numbers)
@@ -46,24 +46,113 @@ void Ex12()
         Console.WriteLine(num);
     }
 }
-void  Ex15()
+void Ex15()
 {
 
     string palindrom = Console.ReadLine();
 
-    if(palindrom.Length % 2 == 1)
+    if (palindrom.Length % 2 == 1)
     {
-        for(int i = 0; i < palindrom.Length; i++)
+        for (int i = 0; i < palindrom.Length; i++)
         {
-            if (palindrom[i] != palindrom[palindrom.Length -1 -i])
-            { 
+            if (palindrom[i] != palindrom[palindrom.Length - 1 - i])
+            {
                 Console.WriteLine("This is not a palindrom.");
                 break;
             }
 
         }
     }
-     
 
+
+}
+
+
+void Ex16()
+{
+    string userInput = Console.ReadLine();
+    string[] dividedUserInput = userInput.Split(' ');
+
+    int number1 = int.Parse(dividedUserInput[0]);
+    int number2 = int.Parse(dividedUserInput[2]);
+
+    if (dividedUserInput[1] == "+")
+    {
+        Console.WriteLine(number1 + number2);
+    }
+    else if (dividedUserInput[1] == "-")
+    {
+        Console.WriteLine(number1 - number2);
+    }
+    else if (dividedUserInput[1] == "*")
+    {
+        Console.WriteLine(number1 * number2);
+    }
+    else if (dividedUserInput[1] == "/")
+    {
+        Console.WriteLine(number1 / number2);
+    }
+    else
+    {
+        Console.WriteLine("Wrong Input!");
+    }
+}
+void Ex17()
+{
+    string randomSequence = Console.ReadLine();
+
+    string input = Console.ReadLine();
+
+    bool containsSearchResult = randomSequence.Contains(input);
+    Console.WriteLine($"Contains \"{input}\"? {containsSearchResult}");
+
+    string[] splitRandomSequence = randomSequence.Split(input);
+    if(splitRandomSequence.Length > 1 ) 
+    {
+        Console.Write(splitRandomSequence[0]);
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write(input);
+        Console.ResetColor();
+        Console.Write(splitRandomSequence[1]);
+    }
+    else
+    {
+        if (splitRandomSequence[0][0] == randomSequence[0])
+        {
+            Console.Write(splitRandomSequence);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(input);
+            Console.ResetColor();
+
+        }else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(input);
+            Console.ResetColor();
+            Console.WriteLine(splitRandomSequence);
+        }
+    }
+   
+}
+
+void Ex18()
+{
+    string userInput = Console.ReadLine();
+    string[] dividedUserInput = userInput.Split(" ");
+    List<int> stringsLength = new List<int>();
+    int sum = 0;
+
+    for(int i = 0; i < dividedUserInput.Length; i++)
+    {
+        stringsLength.Add(dividedUserInput[i].Length);
+
+    }
+         
+    foreach(int i in stringsLength) 
+    {
+        sum += i;
+    }
+
+    Console.WriteLine("Average number of characters in this sentence is: " + Math.Round((double)sum/dividedUserInput.Length));
 }
 
